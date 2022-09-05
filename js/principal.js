942017c94f8d23fb80acc00601c59255df4f3765
JONYCAGE2022 <112737912+JONYCAGE2022@users.noreply.github.com>
2022-09-05 01:50:13 -0300

    repitiendo la validacion y el IMC para todos los pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,38 @@
 
-var paciente = document.querySelector("#primer-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
-var tdPeso = paciente.querySelector(".info-peso");//aqui se accede al hijo "info-peso" a traves del padre "primer paciente"
-var peso = tdPeso.textContent;
+for (var i = 0; i<pacientes.length; i++){
+    var paciente = pacientes[i]
+    var tdPeso = paciente.querySelector(".info-peso");//aqui se accede al hijo "info-peso" a traves del padre "primer paciente"
+    var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdIMC = paciente.querySelector(".info-imc");
+    var tdIMC = paciente.querySelector(".info-imc");
 
-pesoEsValido = true;
-alturaEsValida = true;
+    pesoEsValido = true;
+    alturaEsValida = true;
 
-//ver logica de matematica aplicada a la informática
-//verdadero o falso --> verdadero
-if ((peso < 0) || (peso > 1000)){
-    console.log("Peso incorrecto");
-    tdIMC.textContent = "Peso incorrecto";   //imprime en la tabla el msj  
-    pesoEsValido = false;
+    //ver logica de matematica aplicada a la informática
+    //verdadero o falso --> verdadero
+    if ((peso < 0) || (peso > 1000)){
+        console.log("Peso incorrecto");
+        tdIMC.textContent = "Peso incorrecto";   //imprime en la tabla el msj  
+        pesoEsValido = false;
+    }
+    //verdadero o falso --> verdadero
+    if ((altura < 0) || (altura > 3.00)){
+        console.log("Altura incorrecto");
+        tdIMC.textContent = "Altura incorrecto ";  //imprime en la tabla el msj
+        alturaEsValida = false;
+    }
+    //verdadero Y verdadero --> verdadero
+    // verdadero y falso --> falso
+    if (pesoEsValido && alturaEsValida){
+        var imc = peso / (altura * altura) //100 / 2 * 2 = 100 / 4 = 25 
+        tdIMC.textContent = imc.toFixed(2);
+    }
 }
-//verdadero o falso --> verdadero
-if ((altura < 0) || (altura > 3.00)){
-    console.log("Altura incorrecto");
-    tdIMC.textContent = "Altura incorrecto ";  //imprime en la tabla el msj
-    alturaEsValida = false;
-}
-//verdadero Y verdadero --> verdadero
-// verdadero y falso --> falso
-if (pesoEsValido && alturaEsValida){
-    var imc = peso / (altura * altura) //100 / 2 * 2 = 100 / 4 = 25 
-    tdIMC.textContent = imc;
-}
+
