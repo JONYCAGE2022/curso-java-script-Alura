7696144d777acabe09edd3c06a2be67f6df0a89f
JONYCAGE2022 <112737912+JONYCAGE2022@users.noreply.github.com>
2022-09-04 23:09:52 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -7,14 +7,28 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var tdIMC = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura) //100 / 2 * 2 = 100 / 4 = 25 
-tdIMC.textContent = imc;
+pesoEsValido = true;
+alturaEsValida = true;
 
+//ver logica de matematica aplicada a la informática
+//verdadero o falso --> verdadero
 if ((peso < 0) || (peso > 1000)){
     console.log("Peso incorrecto");
+    tdIMC.textContent = "Peso incorrecto";   //imprime en la tabla el msj  
+    pesoEsValido = false;
 }
 
-if ((altura < 0) || (altura > 4.00)){
+//verdadero o falso --> verdadero
+if ((altura < 0) || (altura > 3.00)){
     console.log("Altura incorrecto");
+    tdIMC.textContent = "Altura incorrecto";  //imprime en la tabla el msj
+    alturaEsValida = false;
+}
+
+//verdadero Y verdadero --> verdadero
+// verdadero y falso --> falso
+if (pesoEsValido && alturaEsValida){
+    var tdIMC = paciente.querySelector(".info-imc");
+    var imc = peso / (altura * altura) //100 / 2 * 2 = 100 / 4 = 25 
+    tdIMC.textContent = imc;
 }
