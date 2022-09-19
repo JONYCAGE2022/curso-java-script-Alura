153f5a5a70778e1657e281f1c6ec31cdd149c69f
JONYCAGE2022 <112737912+JONYCAGE2022@users.noreply.github.com>
2022-09-19 12:51:33 -0300

    falta reviision

--- js/form.js
+++ js/form.js
@@ -11,42 +11,10 @@ var botonAdicionar = document.querySelector("#adicionar-paciente");
 botonAdicionar.addEventListener("click",function(event){ //funcion anonima que se la suele usar en muchos eventos
     event.preventDefault();
 
-    var form = document.querySelector("#form-adicionar");
-
-    var paciente = capturarDatosPacientes(form);
-    console.log(paciente);
-    
-    var tabla = document.querySelector("#tabla-pacientes");
-
-    //crear los tds y un tr
-    pacienteTr = document.createElement("tr");
-    nombreTd = document.createElement("td");
-    alturaTd = document.createElement("td");
-    pesoTd = document.createElement("td"); 
-    gorduraTd = document.createElement("td");
-    imcTd = document.createElement("td");
-
-    //<tr><tr>
-    //<td><td>
-    //<td><td>
-    //<td><td>
-    //<td><td>
-    //<td><td>
-
-    //asignar los valores a la propiedad  textContent
-    nombreTd.textContent = paciente.nombre;//le asigno a esto que creamos anteriormente el valor que el usuario ingreso
-    alturaTd.textContent = paciente.altura;
-    pesoTd.textContent = paciente.peso;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = paciente.imc; 
-
-    //asignacion al tr de los td, y la tabla el tr 
-    pacienteTr.appendChild(nombreTd);//asociamos el hijo nombreTd al padre pacienteTr
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
+    var form = document.querySelector("#form-adicionar");//creando mi formulario
+    var paciente = capturarDatosPacientes(form);//creando mi objeto paciente
+    var pacienteTr = construirTr(paciente);
+    var tabla = document.querySelector("#tabla-pacientes");//creando mi tabla
     tabla.appendChild(pacienteTr);//asociamos pacienteTr a la tabla .. eso quiere decir que los valores capturados se ingresan en la tabla
 
 });
@@ -62,4 +30,47 @@ function capturarDatosPacientes(form){
         imc : calcularIMC(form.peso.value,form.altura.value)
     }
     return paciente;
+}
+
+function construirTr(paciente){
+    //crear los tds y un tr
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    var nombreTd = construirTd(paciente.nombre,"info-nombre");
+    var pesoTd = construirTd(paciente.peso,"info-peso");
+    var alturaTd = construirTd(paciente.altura,"info-altura"); 
+    var gorduraTd = construirTd(paciente.gordura,"info-gordura");
+    var imcTd = construirTd(paciente.imc,"info-imc");
+
+    //<tr><tr>
+    //<td><td>
+    //<td><td>
+    //<td><td>
+    //<td><td>
+    //<td><td>
+
+    /*//asignar los valores a la propiedad  textContent
+    nombreTd.textContent = paciente.nombre;//le asigno a esto que creamos anteriormente el valor que el usuario ingreso
+    alturaTd.textContent = paciente.altura;         esto ya no se lo utiliza, fue reemplazado por la funcion construirTd
+    pesoTd.textContent = paciente.peso;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc; */
+
+    //asignacion al tr de los td, y la tabla el tr 
+    pacienteTr.appendChild(nombreTd);//asociamos el hijo nombreTd al padre pacienteTr
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    return pacienteTr;
+}
+
+function construirTd(dato,clase){
+    var td = document.createElement("td");
+    td.classListt.add(clase);
+    td.textContent = dato;
+
+    return td;
 }
