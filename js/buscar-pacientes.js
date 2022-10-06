472c3388bcb2c6f41f3ef526e9aa530254f0a01f
JONYCAGE2022 <112737912+JONYCAGE2022@users.noreply.github.com>
2022-10-06 02:05:03 -0300

    Conectando y cargando los pacientes

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -2,4 +2,10 @@ var botonBuscar = document.querySelector("#buscar-paciente");
 
 botonBuscar.addEventListener("click",function(){
     console.log("Buscando paciente...");
+    var xhr = new XMLHttpRequest;//XMLHttpRequest es el responsable por realizar las requisiciones HTTP asincronicas con JavaScript.Para instanciar el objeto necesitamos usar la palabra new
+    xhr.open("GET","https://alura-es-cursos.github.io/api-pacientes/pacientes.json");
+    xhr.addEventListener("load",function(){
+        console.log(xhr.responseText);
+    });
+    xhr.send()
 });
