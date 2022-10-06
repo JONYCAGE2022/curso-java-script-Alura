65d43d206b9ea9a076d50346d6886f1e659fa5a6
JONYCAGE2022 <112737912+JONYCAGE2022@users.noreply.github.com>
2022-10-06 02:51:55 -0300

    capturando errores de requerimiento

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -4,12 +4,20 @@ botonBuscar.addEventListener("click",function(){//boton captura el clik
     var xhr = new XMLHttpRequest;//XMLHttpRequest es el responsable por realizar las requisiciones HTTP asincronicas con JavaScript.Para instanciar el objeto necesitamos usar la palabra new
     xhr.open("GET","https://alura-es-cursos.github.io/api-pacientes/pacientes.json");//abro esa requisicion
     xhr.addEventListener("load",function(){//carga la variable respuesta 
-        var respuesta = xhr.responseText;
-        var pacientes = JSON.parse(respuesta);//creo la variable pacientes son los valores de la respuesta , parse convierte 
-        pacientes.forEach(function(paciente){//iterar entre todos mis pacientes  y adiciono el paciente
-            adicionarPacienteEnLaTabla(paciente);
-            console.log(paciente);
-        })
+        var errorAjax = document.querySelector("#error-ajax");
+        if (xhr.status == 200){
+            errorAjax.classList.add("invisible");
+            var respuesta = xhr.responseText;
+            var pacientes = JSON.parse(respuesta);//creo la variable pacientes son los valores de la respuesta , parse convierte 
+            pacientes.forEach(function(paciente){//iterar entre todos mis pacientes  y adiciono el paciente
+                adicionarPacienteEnLaTabla(paciente);
+            });
+        }else{
+            errorAjax.classList.remove("invisible");
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
+        
     });
-    xhr.send()
+    xhr.send();
 });
